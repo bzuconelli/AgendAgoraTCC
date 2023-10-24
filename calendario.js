a0b2c94d5c36fe04876981d12653865a3ad9955a
EduardoBragagnolo <137958511+bzuconelli@users.noreply.github.com>
2023-10-24 20:52:53 -0300

    24.10

---
+++ calendario.js
@@ -0,0 +1,26 @@
+function daysInMonth(month, year) {
+    return new Date(year, month, 0).getDate();
+}
+
+function createCalendar() {
+    let calendar = document.querySelector(".calendar");
+
+    // Defina o mês e ano desejados
+    let month = 10; // Novembro (1 - janeiro, 2 - fevereiro, etc.)
+    let year = 2023;
+
+    let daysInThisMonth = daysInMonth(month, year);
+
+    for (let day = 1; day <= daysInThisMonth; day++) {
+        let dayElement = document.createElement("div");
+        dayElement.classList.add("day");
+        dayElement.innerHTML = `
+        <div class="day h2">
+        <p><strong>${day}</strong></p>
+        <input type="text" class="form-control day-text" placeholder="Vagas para este dia">
+        </div>`;
+        calendar.appendChild(dayElement);
+    }
+}
+
+createCalendar();
